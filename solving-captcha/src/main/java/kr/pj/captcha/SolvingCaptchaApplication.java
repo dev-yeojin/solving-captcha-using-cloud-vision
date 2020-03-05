fd704f59dce5fa34efad93b69f7cddb0deec2638
yeoj1n <yeojinkim94@gmail.com>
2020-03-05 16:35:06 +0900

    project initialize

---
+++ solving-captcha/src/main/java/kr/pj/captcha/SolvingCaptchaApplication.java
@@ -0,0 +1,13 @@
+package kr.pj.captcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolvingCaptchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolvingCaptchaApplication.class, args);
+	}
+
+}
